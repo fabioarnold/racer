7ffd160fdd904391f51459dab4da0f6746ebb4a8
Fabio Arnold <fabio.arnold@img.ly>
2024-07-19 17:51:24 +0200

    Linear steering, fix car dimensions

--- src/main.zig
+++ src/main.zig
@@ -10,10 +10,16 @@ const RL_LINES = 0x0001;
 const RL_TRIANGLES = 0x0004;
 const RL_QUADS = 0x0007;
 
+const max_forward_speed = 15;
+const max_reverse_speed = 5;
+
+const kmph_to_mps = 1.0 / 3.6;
+const mazda_rx7_top_speed_kmph = 250;
+
 const Car = struct {
-    const dist_front = 1.997;
-    const dist_back = 1.283;
-    const wheel_radius = 0.470976;
+    const dist_front = 1.49603;
+    const dist_back = 0.961294;
+    const wheel_radius = 0.352841;
 
     front: Vec3,
     back: Vec3,
@@ -98,6 +104,8 @@ pub fn main() !void {
     const background_texture = rl.loadTextureFromImage(background_image);
     rl.unloadImage(background_image);
 
+
+    var steer: f32 = 0;
     var car = Car.init(Vec3.zero());
 
     var menu_time: f32 = 1000; // @floatCast(rl.getTime());
@@ -112,20 +120,21 @@ pub fn main() !void {
             menu_time = time;
             rl.playSound(sfx_click);
         }
-        var steer: f32 = 0;
-        if (rl.isKeyDown(.key_left)) steer -= 1;
-        if (rl.isKeyDown(.key_right)) steer += 1;
+        if (rl.isKeyDown(.key_left)) steer -= 0.1;
+        if (rl.isKeyDown(.key_right)) steer += 0.1;
+        if (!rl.isKeyDown(.key_left) and !rl.isKeyDown(.key_right) and @abs(steer) > 0.01) steer -= 0.1 * std.math.sign(steer);
+        steer = std.math.clamp(steer, -1, 1);
 
         if (rl.isKeyDown(.key_up)) car.speed += 0.2;
         if (rl.isKeyDown(.key_down)) car.speed -= 0.2;
-        car.speed = std.math.clamp(car.speed, -20, 20);
+        car.speed = std.math.clamp(car.speed, -max_reverse_speed, max_forward_speed);
 
         const max_steering_angle = 0.4;
         car.steering_angle = -max_steering_angle * steer;
         car.integrate(1.0 / 60.0);
 
         camera.target = car.center();
-        camera.position = camera.target.add(Vec3.init(0, 5, 10));
+        camera.position = camera.target.add(Vec3.init(0, 2, 5));
 
         rl.beginDrawing();
         defer rl.endDrawing();
@@ -160,7 +169,9 @@ pub fn main() !void {
             const anim = &model_animations[1];
             const wheel_rot = rlm.quaternionFromAxisAngle(Vec3.init(1, 0, 0), car.wheel_angle);
             const wheel_turn = rlm.quaternionFromAxisAngle(Vec3.init(0, 1, 0), car.steering_angle);
+            anim.framePoses[0][1].rotation = wheel_turn;
             anim.framePoses[0][2].rotation = rlm.quaternionMultiply(wheel_turn, wheel_rot);
+            anim.framePoses[0][3].rotation = wheel_turn;
             anim.framePoses[0][4].rotation = rlm.quaternionMultiply(wheel_turn, wheel_rot);
             anim.framePoses[0][5].rotation = wheel_rot;
             anim.framePoses[0][6].rotation = wheel_rot;
@@ -184,7 +195,9 @@ pub fn main() !void {
         }
 
         rl.drawRectangleLines(40, screen_height / 2 - 300, 40, 600, rl.Color.white);
-        rl.drawRectangle(40, screen_height / 2 - @as(i32, @intFromFloat(15 * @max(0, car.speed))), 40, @intFromFloat(15 * @abs(car.speed)), rl.Color.white);
+        rl.drawRectangle(40, @intFromFloat(screen_height / 2 - 15 * @max(0, car.speed)), 40, @intFromFloat(15 * @abs(car.speed)), rl.Color.white);
+        rl.drawRectangleLines(100, 40, 200, 40, rl.Color.white);
+        rl.drawRectangle(@intFromFloat(200 - 100 * @max(0, -steer)), 40, @intFromFloat(100 * @abs(steer)), 40, rl.Color.white);
 
         const alpha: f32 = @max(0, @min(1, 1 * (time - menu_time)));
         Menu.draw(alpha);
